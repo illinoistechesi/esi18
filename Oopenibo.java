cdb6370230d7799d729539478b0204d96a307621
Nick Li <hli109@hawk.iit.edu>
2018-07-31 01:53:39 -0500

    uncomment long running code caused by while loop

--- Oopenibo.java
+++ Oopenibo.java
@@ -30,41 +30,40 @@ public class Oopenibo extends Ship {
         int y = location.getY();
         this.fire(arena, x+1, y);
         
-        int numMoves = this.getRemainingMoves();
+        // int numMoves = this.getRemainingMoves();
 
-         while(numMoves > 0) {
+        // this while loop is causing some bugs, uncommenting for now
+         // while(numMoves > 0) {
             // move north, west until there's only one turn left
            this.move(arena, Direction.NORTH);
            this.move(arena, Direction.EAST);
             // update the number of turn remaining
-            numMoves = this.getRemainingMoves();
-        }
+            // numMoves = this.getRemainingMoves();
+        // }
     
         List<Ship> nearby = this.getNearbyShips(arena);
         for (int i = 0; i < nearby.size(); i++) {
-        Ship other = nearby.get(i);
-        boolean isOnMyTeam = this.isSameTeamAs(other);
-        if (isOnMyTeam) {
-            // System.out.println("This ship is on my team!");
-        } 
-        else {
-            // System.out.println("This ship is an enemy.");
-            Ship enemy = nearby.get(i);
-            Coord enemyLoc = enemy.getCoord();
-             int a = enemyLoc.getX();
-            int b = enemyLoc.getY();
-            this.fire(arena, a,b);
-        }
-
-        
+            Ship other = nearby.get(i);
+            boolean isOnMyTeam = this.isSameTeamAs(other);
+            if (isOnMyTeam) {
+                // System.out.println("This ship is on my team!");
+            } 
+            else {
+                // System.out.println("This ship is an enemy.");
+                Ship enemy = nearby.get(i);
+                Coord enemyLoc = enemy.getCoord();
+                int a = enemyLoc.getX();
+                int b = enemyLoc.getY();
+                this.fire(arena, a,b);
+            }
         }
         //Determines if ship b is within the range of ship a.
 //Ship a: ship to check range from
 //Ship b: ship to check for
 //Returns: boolean
-Direction[] possibleMovement = {Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST};
-int randomNumber = arena.getRandom().nextInt(2);
-    this.move(arena, possibleMovement[randomNumber]);
+// Direction[] possibleMovement = {Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST};
+// int randomNumber = arena.getRandom().nextInt(2);
+//     this.move(arena, possibleMovement[randomNumber]);
     }
 
 
